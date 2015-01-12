f8724d026db7db6088e9660d8fb4bbe2a580dd64
Nathan Hall <nathan.mh@gmail.com>
2015-01-11 18:29:14 -0600

    basic index question

---
+++ lib/lg_index.rb
@@ -0,0 +1,21 @@
+def largest_index(array)
+  if array.length < 1
+    return false
+  end
+  lg_index = 0
+  array.each_index do |ind|
+    if array[ind] > array[lg_index]
+      lg_index = ind
+    end
+  end
+  lg_index
+end
+
+b = [3,-1,6,100,3,5,9,-2]
+c = []
+d = [-1,-2,-3,-4,-5]
+
+puts largest_index(b)
+puts largest_index(c)
+puts largest_index(d)
+
