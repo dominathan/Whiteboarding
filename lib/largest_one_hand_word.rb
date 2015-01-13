b96acb9a6b957c83f2c45175a536cdfa5e295c4d
Nathan Hall <nathan.mh@gmail.com>
2015-01-13 13:53:42 -0600

    largest one handed word

---
+++ lib/largest_one_hand_word.rb
@@ -0,0 +1,9 @@
+# Given a dictionary, what is the largest word you can type using only your left hand
+require_relative '../tale_of_two_cities.rb'
+
+class LargestWord
+  DICT = TALE_OF_TWO_CITIES.split(" ")
+  hand = {left: "qwertyasdfgzxcv", right: 'uiophjklbnm'}
+
+
+end
